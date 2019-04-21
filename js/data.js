795fb8b7b3b525757aa6fc7b7046f6b1b2387382
Paul Schot <pks5885@gmail.com>
2019-04-20 17:17:20 -0700

    Digitizer.2.0
    * New look and layout
    * Adds CSV download button

---
+++ js/data.js
@@ -0,0 +1,185 @@
+// DATASET TYPES
+var TYPE_START = 0;
+var TYPE_BARBELL = 1;
+var TYPE_LIFT = 2;
+var TYPE_STS = 3;
+var TYPE_BALL_TOSS = 4;
+var TYPE_JAR_DN = 5;
+var TYPE_JAR_UP = 6;
+var TYPE_JUMP_OUT = 7;
+var TYPE_JUMP_UP = 8;
+var TYPE_OBSTACLE = 9;
+
+// SUBJECT TYPES
+var TYPE_TALL = 0;
+var TYPE_SHORT = 1;
+
+var datasets;
+
+function initData() {
+    datasets = [
+        new DataSetNoSub("Sprint Start",
+            ["LTOE", "RTOE", "LANK", "RANK", "LKNE", "RKNE", "LHIP", "RHIP", "LSHL", "RSHL", "LELB", "RELB", "LWRI", "RWRI", "EAR"],
+            ["Left toe", "Right toe", "Left ankle", "Right ankle", "Left knee", "Right knee", "Left hip", "Right hip",
+                "Left shoulder", "Right shoulder", "Left elbow", "Right elbow", "Left wrist", "Right wrist", "Ear"],
+            1, "more", "Sprinter", 290, 356
+        ),
+        new DataSetNoSub("Barbell Lift",
+            ["REF", "HEEL", "TOE", "ANK", "KNE", "HIP", "SHL", "EAR", "BAR"],
+            ["Reference", "Heel", "Toe", "Ankle", "Knee", "Hip", "Shoulder", "Ear", "Bar"],
+            1, "more", "OlympicLift", 169, 405
+        ),
+        new DataSetNoSub("Box Lift",
+            ["TOE", "MFT", "HEEL", "ANK", "KNE", "HIP", "SHL", "WRI", "EAR", "BOX UL", "BOX BR"],
+            ["Toe", "Midfoot", "Heel", "Ankle", "Knee", "Hip", "Shoulder", "Wrist", "Ear", "Box (upper left)", "Box (bottom right)"],
+            1, "more", "BoxLift", 224, 315
+        ),
+        new DataSetNoSub("Sit to Stand",
+            ["REFL", "REFR", "TOE", "HEEL", "ANK", "KNE", "HIP", "SHL", "ELB", "WRI", "EAR"],
+            ["Reference( left)", "Reference (right)", "Toe", "Heel", "Ankle", "Knee", "Hip", "Shoulder", "Elbow", "Wrist", "Ear"],
+            3, "STS", "STS", 336, 472
+        ),
+        new DataSet("Ball Toss",
+            ["REFBR", "REFTL", "EAR", "HIP", "KNE", "ANK", "TOE", "BALL", "WRI", "ELB", "SHL"],
+            ["Reference (bottom right)", "Reference (top left)", "Ear", "Hip", "Knee", "Ankle", "Toe", "Ball", "Wrist", "Elbow", "Shoulder"],
+            [16, 14], "BALL_TOSS", "BT", 500, 281
+        ),
+        new DataSet("Jar Down",
+            ["REFBR", "REFTL", "EAR", "HIP", "KNE", "ANK", "TOE", "JAR", "WRI", "ELB", "SHL"],
+            ["Reference (bottom right)", "Reference (top left)", "Ear", "Hip", "Knee", "Ankle", "Toe", "Jar", "Wrist", "Elbow", "Shoulder"],
+            [11, 12], "JAR_DN", "LO", 500, 281
+        ),
+        new DataSet("Jar Up",
+            ["REFBR", "REFTL", "EAR", "HIP", "KNE", "ANK", "TOE", "JAR", "WRI", "ELB", "SHL"],
+            ["Reference (bottom right)", "Reference (top left)", "Ear", "Hip", "Knee", "Ankle", "Toe", "Jar", "Wrist", "Elbow", "Shoulder"],
+            [13, 11], "JAR_UP", "RO", 500, 281
+        ),
+        new DataSet("Jump Out",
+            ["REFBR", "REFTL", "TOE", "ANK", "KNE", "HIP", "SHL", "EAR"],
+            ["Reference (bottom right)", "Reference (top left)", "Toe", "Ankle", "Knee", "Hip", "Shoulder", "Ear"],
+            [20, 20], "JUMP_OUT", "HJ", 500, 281
+        ),
+        new DataSet("Jump Up",
+            ["REFBR", "REFTL", "TOE", "ANK", "KNE", "HIP", "SHL", "EAR"],
+            ["Reference (bottom right)", "Reference (top left)", "Toe", "Ankle", "Knee", "Hip", "Shoulder", "Ear"],
+            [15, 18], "JUMP_UP", "VJ", 500, 281
+        ),
+        new DataSet("Obstacle",
+            ["REFBR", "REFTL", "TOE", "ANK", "KNE", "HIP", "SHL", "EAR"],
+            ["Reference (bottom right)", "Reference (top left)", "Toe", "Ankle", "Knee", "Hip", "Shoulder", "Ear"],
+            [12, 13], "OBSTACLE", "SB", 500, 281
+        )
+    ];
+}
+
+function DataSet(name, headers, headersLong, numImages, imgDir, img, w, h) {
+    this.name = name;
+    this.headers = headers;
+    this.headersLong = headersLong;
+    this.numImages = numImages;
+    this.imageDir = imgDir
+    this.image = img
+    this.imgWidth = w;
+    this.imgHeight = h;
+
+    this.getNumberOfImages = function(sub) {
+        return this.numImages[sub];
+    };
+    this.hasSub = true;
+}
+
+function DataSetNoSub(name, headers, headersLong, numImages, imgDir, img, w, h) {
+    this.name = name;
+    this.headers = headers;
+    this.headersLong = headersLong;
+    this.numImages = numImages;
+    this.imageDir = imgDir
+    this.image = img
+    this.imgWidth = w;
+    this.imgHeight = h;
+
+    this.getNumberOfImages = function() {
+        return this.numImages;
+    };
+    this.hasSub = false;
+}
+
+function getDataSet(datasetID) {
+    return datasets[datasetID];
+}
+
+function getSubCategory(value) {
+    if (value == TYPE_TALL) {
+        return "T";
+    }
+    if (value == TYPE_SHORT){
+        return "S";
+    }
+    return "";
+}
+
+function table2CSV(table, dataset) {
+    var csvData = [];
+
+    // header data
+    var tmpRow = [];
+    tmpRow[tmpRow.length] = "Frame";
+    for (var h = 0; h < dataset.headers.length; h++) {
+        tmpRow[tmpRow.length] = dataset.headers[h] + " X";
+        tmpRow[tmpRow.length] = dataset.headers[h] + " Y";
+    }
+    row2CSV(tmpRow);
+
+    // actual data
+    var tableBody = table.getElementsByTagName("tbody")[0];
+    var tableRows = tableBody.getElementsByTagName("tr");
+    for (var r = 0; r < tableRows.length; r++) {
+        var tableRow = tableRows[r];
+        var tmpRow = [];
+        var cells = tableRow.getElementsByTagName("td");
+        for (var c = 0; c < cells.length; c++) {
+            tmpRow[tmpRow.length] = formatData(cells[c]);
+        }
+        row2CSV(tmpRow);
+    }
+
+    var mydata = csvData.join('\n');
+    var filename = dataset.image + getSubCategory(currentSubCategory) + ".csv";
+    download(mydata, filename);
+
+    function row2CSV(tmpRow) {
+        var tmp = tmpRow.join('') // to remove any blank rows
+        if (tmpRow.length > 0 && tmp) {
+            var mystr = tmpRow.join(',');
+            csvData[csvData.length] = mystr;
+        }
+    }
+
+    function formatData(cell) {
+        var output = cell.innerText;
+        if (!output) return '';
+        return output.trim();
+    }
+
+    function popup(data) {
+        var generator = window.open('', 'csv', 'height=400,width=600');
+        generator.document.write('<html><head><title>CSV</title>');
+        generator.document.write('</head><body >');
+        generator.document.write('<textArea cols=70 rows=15 wrap="off" >');
+        generator.document.write(data);
+        generator.document.write('</textArea>');
+        generator.document.write('</body></html>');
+        generator.document.close();
+        return true;
+    }
+
+    function download(data, filename) {
+        var blob = new Blob([data], {type: "text/csv"});
+        var a = document.createElement('a');
+        a.setAttribute("href", window.URL.createObjectURL(blob));
+        a.setAttribute("download", filename);
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+};
---
+++ js/scripts.js
@@ -0,0 +1,258 @@
+var currentDataSet;
+var currentSubCategory;
+var currentImage = 0;
+var lastClick = [0, 0];
+
+function startup() {
+    initData();
+    initMenus();
+    selectFirstCategoryOption();
+    changeImage();
+    document.addEventListener("click", function(event) {
+        closeMenus(event);
+    });
+    document.getElementById("img").addEventListener("mousedown", imgClick);
+}
+
+function initMenus() {
+    var optionsContainer = document.getElementById("categoryOptions");
+    var optionTemplate = optionsContainer.getElementsByTagName("li")[0];
+    optionsContainer.innerHTML = "";
+    for (var i = 0; i < datasets.length; i++) {
+        var option = datasets[i];
+        var newOption = optionTemplate.cloneNode(true);
+        var child = newOption.firstElementChild;
+        child.setAttribute("value", i);
+        child.innerHTML = option.name;
+        optionsContainer.appendChild(newOption);
+    }
+}
+
+function selectFirstCategoryOption() {
+    var options = document.getElementById("categoryOptions");
+    var firstOpt = options.getElementsByClassName("subMenuItem")[0];
+    categorySelect(firstOpt);
+}
+
+function selectFirstSubCategoryOption() {
+    if (currentDataSet.hasSub) {
+        var options = document.getElementById("subCategoryOptions");
+        var firstOpt = options.getElementsByClassName("subMenuItem")[0];
+        subCategorySelect(firstOpt);
+    } else {
+        currentSubCategory = -1;
+    }
+}
+
+function toggleMenu(button) {
+    var parent = button.parentElement;
+    if (hasClass(parent, "disabled")) {
+        parent.classList.remove("open");
+        return;
+    }
+    parent.classList.toggle("open");
+}
+
+function categorySelect(choice) {
+    currentDataSet = getDataSet(choice.getAttribute("value"));
+    toggleSelected(document.getElementById("categoryOptions"), choice);
+    selectFirstSubCategoryOption();
+    updateCategoryDisplay();
+}
+
+function subCategorySelect(choice) {
+    currentSubCategory = choice.getAttribute("value");
+    toggleSelected(document.getElementById("subCategoryMenu"), choice);
+    updateCategoryDisplay();
+}
+
+function updateCategoryDisplay() {
+    document.getElementById("currentCategory").innerHTML = currentDataSet.name;
+    if (currentDataSet.hasSub) {
+        if (currentSubCategory == TYPE_TALL) {
+            document.getElementById("currentSubCategory").innerHTML = "Tall";
+        } else {
+            document.getElementById("currentSubCategory").innerHTML = "Short";
+        }
+        document.getElementById("subCategoryMenu").classList.remove("disabled");
+    } else {
+        document.getElementById("currentSubCategory").innerHTML = "";
+        document.getElementById("subCategoryMenu").classList.add("disabled");
+        document.getElementById("subCategoryMenu").classList.remove("open");
+    }
+    currentImage = 1;
+    changeImage();
+    initTable();
+}
+
+function toggleSelected(menu, choice) {
+    var options = menu.getElementsByClassName("subMenuItem");
+    for (var i = 0; i < options.length; i++) {
+        var option = options[i];
+        option.classList.remove("selected");
+    }
+    choice.classList.add("selected");
+}
+
+function hasClass(elem, classname) {
+    return elem.classList.contains(classname);
+}
+
+function initTable() {
+    var table = document.getElementById("dataTable");
+    table.innerHTML = "";
+    var thead = document.createElement("thead")
+    var tbody = document.createElement("tbody");
+    var tr;
+    var th;
+    var td;
+    var headRow1 = document.createElement("tr");
+    var headRow2 = document.createElement("tr");
+    th = document.createElement("th");
+    th.setAttribute("rowspan", "2");
+    th.innerHTML = "Frame";
+    headRow1.appendChild(th);
+    for (var i = 0; i < currentDataSet.headers.length; i++) {
+        th = document.createElement("th");
+        th.setAttribute("colspan", "2");
+        th.innerHTML = currentDataSet.headers[i];
+        th.setAttribute("title", currentDataSet.headersLong[i]);
+        headRow1.appendChild(th);
+        th = document.createElement("th");
+        th.innerHTML = "x";
+        headRow2.appendChild(th);
+        th = document.createElement("th");
+        th.innerHTML = "y";
+        headRow2.appendChild(th);
+    }
+    thead.appendChild(headRow1);
+    thead.appendChild(headRow2);
+    for (var i = 0; i < currentDataSet.getNumberOfImages(currentSubCategory); i++) {
+        tr = document.createElement("tr");
+        if (i == 0) {
+            tr.classList.add("current");
+        }
+        td = document.createElement("td");
+        td.innerHTML = "" + (i + 1);
+        tr.appendChild(td);
+        for (var j = 0; j < currentDataSet.headers.length; j++) {
+            td = document.createElement("td");
+            td.setAttribute("row", "" + i);
+            td.setAttribute("col", "" + j);
+            td.onmouseover = function() { tableHover(this); };
+            td.onmouseleave = function() { tableLeave(this); };
+            td.onclick = function() { tableClick(this); };
+            tr.appendChild(td);
+            td = document.createElement("td");
+            td.setAttribute("row", "" + i);
+            td.setAttribute("col", "" + j);
+            td.onmouseover = function() { tableHover(this); };
+            td.onmouseleave = function() { tableLeave(this); };
+            td.onclick = function() { tableClick(this); };
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    table.appendChild(thead);
+    table.appendChild(tbody);
+}
+
+function tableHover(cell) {
+    var row = parseInt(cell.getAttribute("row"));
+    var col = 1 + 2 * parseInt(cell.getAttribute("col"));
+    var table = document.getElementById("dataTable");
+    var tbody = table.getElementsByTagName("tbody")[0];
+    tbody.rows[row].cells[col].classList.add("highlight");
+    tbody.rows[row].cells[col + 1].classList.add("highlight");
+}
+
+function tableLeave(cell) {
+    var row = parseInt(cell.getAttribute("row"));
+    var col = 1 + 2 * parseInt(cell.getAttribute("col"));
+    var table = document.getElementById("dataTable");
+    var tbody = table.getElementsByTagName("tbody")[0];
+    tbody.rows[row].cells[col].classList.remove("highlight");
+    tbody.rows[row].cells[col + 1].classList.remove("highlight");
+}
+
+function tableClick(cell) {
+    var row = parseInt(cell.getAttribute("row"));
+    var col = 1 + 2 * parseInt(cell.getAttribute("col"));
+    if (lastClick[0] && lastClick[1]) {
+        if ((row + 1) == currentImage) {
+            var table = document.getElementById("dataTable");
+            var tbody = table.getElementsByTagName("tbody")[0];
+            tbody.rows[row].cells[col].innerHTML = lastClick[0];
+            tbody.rows[row].cells[col + 1].innerHTML = lastClick[1];
+        }
+    }
+}
+
+function closeMenus(event) {
+    var targetElement = event.target;
+    var menu = document.getElementById("menu");
+    do {
+        if (targetElement == menu) {
+            return;
+        }
+        targetElement = targetElement.parentNode;
+    } while (targetElement);
+
+    var categoryMenu = document.getElementById("categoryMenu");
+    var subCategoryMenu = document.getElementById("subCategoryMenu");
+    categoryMenu.classList.remove("open");
+    subCategoryMenu.classList.remove("open");
+}
+
+function changeImage() {
+    var img = document.getElementById("img");
+    var source = "./images/" + currentDataSet.imageDir + "/" + getSubCategory(currentSubCategory) + currentDataSet.image;
+    if (currentDataSet.getNumberOfImages(currentSubCategory) > 1) {
+        source += "" + currentImage;
+    }
+    source += ".jpg";
+    img.src = source;
+    img.setAttribute("width", currentDataSet.imgWidth);
+    img.setAttribute("height", currentDataSet.imgHeight);
+    var tbody = document.getElementsByTagName("tbody")[0];
+    var rows = tbody.getElementsByTagName("tr");
+    for (var i = 0; i < rows.length; i++) {
+        rows[i].classList.remove("current");
+        if ((i + 1) == currentImage) {
+            rows[i].classList.add("current");
+        }
+    }
+    document.getElementById("currentImage").innerHTML = currentImage;
+    document.getElementById("totalImages").innerHTML = currentDataSet.getNumberOfImages(currentSubCategory);
+}
+
+function nextImage() {
+    if (currentImage < currentDataSet.getNumberOfImages(currentSubCategory)) {
+        currentImage++;
+    } else {
+        currentImage = 1;
+    }
+    changeImage();
+}
+
+function prevImage() {
+    if (currentImage > 1) {
+        currentImage--;
+    } else {
+        currentImage = currentDataSet.getNumberOfImages(currentSubCategory);
+    }
+    changeImage();
+}
+
+function imgClick(evt) {
+    var img = document.getElementById("img");
+    var rect = img.getBoundingClientRect();
+    lastClick[0] = Math.round(evt.clientX - rect.left);
+    lastClick[1] = Math.round(rect.bottom - evt.clientY);
+    document.getElementById("lastX").innerHTML = lastClick[0];
+    document.getElementById("lastY").innerHTML = lastClick[1];
+}
+
+function generateCSV() {
+    table2CSV(document.getElementById("dataTable"), currentDataSet);
+}
